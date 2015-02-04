a3e672531650cc6ed6dce36de0d39162f1c528b2
Balcmeg <stefangutehall@Stefans-iMac.local>
2015-02-04 12:13:42 +0800

    Rewrote plot1 in qqplot

--- plot1.R
+++ plot1.R
@@ -5,5 +5,12 @@ powerdata<-openfile()
 
 ## Plotting graph to file
 png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
-hist(powerdata$Global_active_power,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+p1<-ggplot(powerdata, aes(x=Global_active_power))
+p1<-p1+geom_histogram(,binwidth=0.5,origin=0,color="black",fill="red")
+p1<-p1+labs(x="Global Active Power (kilowatts)", y="Frequency")
+p1<-p1+ggtitle("Global Active Power")
+p1<-p1+xmax(0,6)
+p1<-p1+theme(panel.grid = element_blank(),panel.background = element_blank())
+p1<-p1+theme(axis.line=element_line(color="black"))
+print(p1)
 dev.off()
