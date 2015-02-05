6a9e32926c5a12c869b6d65f1796ece6473f472d
Balcmeg <stefangutehall@Stefans-iMac.local>
2015-02-05 13:42:45 +0800

    Changed the plots back to plot from ggplot
    To make sure they look identical to the reference graphs. ggplot look
    slightly different

--- plot2.R
+++ plot2.R
@@ -6,13 +6,8 @@ library(scales)
 
 ## Plotting graph
 png(filename = "plot2.png", width = 480, height = 480, bg = "transparent")
-p2<-ggplot(powerdata, aes(x=DateTime, y=Global_active_power))
-p2<-p2+geom_line(color="black")
-p2<-p2+labs(x="",y="Global Active Power (kilowatts)")
-p2<-p2+theme(panel.grid = element_blank(),panel.background = element_blank())
-p2<-p2+theme(axis.line=element_line(color="black"))
-#p2<-p2+scale_x_date(labels = as.Date(powerdata$DateTime, format="%A"))
-##p2<-p2+scale_x_date(labels = weekdays(powerdata$DateTime))
-p2<-p2+scale_x_datetime(breaks = "1 day",labels = date_format("%A"))
+p2=plot(powerdata$Global_active_power~powerdata$DateTime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
 print(p2)
 dev.off()
+
--- plot3.R
+++ plot3.R
@@ -3,10 +3,11 @@
 source("openfile.R")
 powerdata<-openfile()
 
-## Plotting graph to file
+## Plotting graph
 png(filename = "plot3.png", width = 480, height = 480, bg = "transparent")
-plot(powerdata$DateTime,powerdata$Sub_metering_1,type="l",xlab = "",ylab = "Energy sub metering",col="black")
-lines(powerdata$DateTime, powerdata$Sub_metering_2, col = "red")
-lines(powerdata$DateTime, powerdata$Sub_metering_3, col = "blue")
-legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 1)
+p3<-plot(powerdata$Sub_metering_1~powerdata$DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+p3<-p3+lines(powerdata$Sub_metering_2~powerdata$DateTime,col='Red')
+p3<-p3+lines(powerdata$Sub_metering_3~powerdata$DateTime,col='Blue')
+p3<-p3+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,  legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+print(p3)
 dev.off()
