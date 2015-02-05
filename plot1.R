e141798d9e4cc9dcb8adfb9b87a13a4e26f3bed5
Balcmeg <stefangutehall@Stefans-iMac.local>
2015-02-05 14:07:03 +0800

    Should be final commit now all is working

--- plot1.R
+++ plot1.R
@@ -6,5 +6,4 @@ powerdata<-openfile()
 ## Plotting graph to file
 png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
 hist(powerdata$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-print(p1)
 dev.off()
